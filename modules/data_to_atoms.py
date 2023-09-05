676e4e50834505c4dc717d1271b6d036a9f5d121
mavaylon1 <mavaylon1@berkeley.edu>
2023-09-05 10:30:36 -0700

    generalize energy and forces

--- modules/data_to_atoms.py
+++ modules/data_to_atoms.py
@@ -3,18 +3,24 @@ import copy
 import numpy as np
 
 
-def build_atoms(data: dict) -> ase.Atoms:
+def build_atoms(data: dict
+                energy: str = None,
+                forces: str = None) -> ase.Atoms:
     """ 
     Populate Atoms class with atoms in molecule.
         atoms.info : global variables
         atoms.array : variables for individual atoms
+        
+    Both "energy" and "forces" are the dict strings in data.
     """
 
     atoms = ase.atoms.Atoms(
         symbols=data['elements'],
         positions=data['positions']
     )
-    atoms.info['energy'] = data['energy_internal_0K']
-    #atoms.arrays['positions'] = data['positions']
-
+    if energy is not None:
+        atoms.info['energy'] = data[energy]
+    if forces not None:
+        atoms.info['forces'] = data[forces]
+        
     return copy.copy(atoms)
